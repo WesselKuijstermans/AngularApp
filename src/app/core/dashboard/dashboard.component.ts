33d8e45810d32d5f5f7ed28d9cd65010054aa211
Robin Schellius <r.schellius@avans.nl>
2019-11-22 19:36:27 +0100

    Updated for production mode

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  runningMode: string
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.runningMode = environment.production ? 'production' : 'development'
+  }
 }

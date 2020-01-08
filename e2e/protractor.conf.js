02ba71e257307a38e693b4f52def6023da54913e
Robin Schellius <r.schellius@avans.nl>
2020-01-08 13:34:04 +0100

    [fix] updating e2e tests

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,7 +13,8 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--no-sandbox', '--versions.chrome 76.0.3809.6']
+      // args: ['--headless', '--no-sandbox', '--versions.chrome 76.0.3809.6']
+      args: ['--headless', '--no-sandbox']
     }
   },
   directConnect: true,

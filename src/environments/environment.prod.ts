710f756324fda80353e434a1a81a747925e6aeb2
Wessel Kuijstermans <w.kuijstermans@home.nl>
2021-11-18 23:04:17 +0100

    Swapping environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@ export const environment = {
   production: true,
 
   // Fill in your own online server API url here
-  apiUrl: 'https://jouw-eigen-server.herokuapp.com/api',
+  // apiUrl: 'https://jouw-eigen-server.herokuapp.com/api',
+  apiUrl: 'https://localhost:3000',
 
   version: packagejson.version
 }
